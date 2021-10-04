e4e965ce6311279eb02936e539148505ec9bae97
bpantoja4 <91565762+bpantoja4@users.noreply.github.com>
2021-10-04 15:02:41 -0500

    Add files via upload

---
+++ Colors.py
@@ -0,0 +1,9 @@
+#Initialize Turtle
+import turtle
+import random
+brianna=turtle.Turtle()
+def randomDot():
+    brianna.color("blue")
+    brianna.begin_fill("red")
+    brianna.end_fill("red")
+    brianna.circle(100)
