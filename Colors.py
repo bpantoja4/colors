1969c1392635ba1bc34c6db99044cb67551a51c9
bpantoja4 <91565762+bpantoja4@users.noreply.github.com>
2021-10-06 13:39:49 -0500

    Update Colors.py

--- Colors.py
+++ Colors.py
@@ -1,9 +1,20 @@
-#Initialize Turtle
+#Initialization
 import turtle
 import random
 brianna=turtle.Turtle()
+brianna.colormode(255)
+r = random.randint(0,255)
+g = random.randint(0,255)
+b = random.randint(0,255)
+#Functions
 def randomDot():
-    brianna.color("blue")
-    brianna.begin_fill("red")
-    brianna.end_fill("red")
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    brianna.color(r,g,b)
+    brianna.begin_fill()
     brianna.circle(100)
+    brianna.end_fill()
+#Main
+for i in range(5):
+	randomDot()
